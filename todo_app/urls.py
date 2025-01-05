083297b9b2d973caa4bf6aaa46966f5dc4babe12
Wallacy V. <wlcsv8080@outlook.com>
2025-01-04 21:05:02 -0300

    Tentar terminar isso pelo PC, é mais agradável de se trabalhar por lá

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,2 +1,8 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
+    path("", views.ListListView, name="index"),
+    path("list/<int:list_id>/",
+         viewd.ItemListview, name="list"),
 ]
